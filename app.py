2513625a60e338dd13da89d9cdfcb5caff6c44c6
thispersonisbrand-alt <thispersonisbrand@gmail.com>
2026-06-04 23:09:20 +0600

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def get_domain():
 
 def create_temp_mail():
     domain = get_domain()
-    username = "user" + str(random.randint(100000000, 999999999))
+    username = "Thispersonisbrand" + str(random.randint(100000000, 999999999))
     email = f"{username}@{domain}"
     password = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
     
